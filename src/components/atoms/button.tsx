b131a045e06d4b9ba8f63f16a81d83910555eaec
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-12-06 15:38:50 +0700

    refactor: button component

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -5,25 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/classnames";
 
 const buttonVariants = cva(
-  "focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 md:font-semibold",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
-        outline:
-          "border-input hover:bg-accent hover:text-accent-foreground border bg-transparent shadow-sm",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "bg-title text-white",
+        outline: "border border-title bg-white text-title",
+        ghost: "hover:bg-title hover:text-white",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        default: "px-4 py-2 text-sm md:px-6 md:text-base lg:px-8",
+        sm: "px-5 py-2 text-sm",
+        lg: "px-4 py-2 leading-7 md:px-6 md:text-2xl md:leading-8 lg:px-8 lg:py-4",
         icon: "h-9 w-9",
       },
     },
