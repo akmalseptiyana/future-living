8c5d7bda59112584accc6ce473d941aa02f6ee78
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-12-08 13:47:41 +0700

    refactor: hero image

--- src/components/organisms/home/hero/featured-image.tsx
+++ src/components/organisms/home/hero/featured-image.tsx
@@ -6,7 +6,7 @@ import { FeaturedBar } from "@/components/molecules/featured-bar";
 export function FeaturedImage() {
   return (
     <div className="relative w-full lg:w-fit">
-      <figure className="relative h-[275px] w-full overflow-hidden rounded md:h-[524px] md:rounded-lg lg:h-[640px] lg:w-[622px]">
+      <figure className="relative h-[275px] w-full overflow-hidden rounded md:h-[524px] md:rounded-lg lg:h-[640px] lg:w-[454px] xl:w-[622px]">
         <Image
           src="https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=692&q=80"
           alt="house"
